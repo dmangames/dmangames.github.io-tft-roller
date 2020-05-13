ed37e4bf60b686de1729511b205c98aeb4a4f22d
dmangames <talktodmangames@gmail.com>
2020-05-12 23:37:52 -0500

    Updates

---
+++ precache-manifest.e1a10ffda9e7ff0be38cceee9d269724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027d920b4ea521b330ba1dfde59884c7",
+    "url": "/tft-roller/index.html"
+  },
+  {
+    "revision": "88a8660d5fa9923b7444",
+    "url": "/tft-roller/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79598bc806acdc8cfba0",
+    "url": "/tft-roller/static/js/2.79e2c185.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tft-roller/static/js/2.79e2c185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a8660d5fa9923b7444",
+    "url": "/tft-roller/static/js/main.5312c94d.chunk.js"
+  },
+  {
+    "revision": "e45b0f03f1daa622dd85",
+    "url": "/tft-roller/static/js/runtime-main.bad47ce7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tft-roller/static/media/logo.5d5d9eef.svg"
+  }
+]);
